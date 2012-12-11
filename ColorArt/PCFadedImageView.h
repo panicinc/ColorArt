820d074073960e9e2175fca377b0946cd61e2de0
Aaron Brethorst <aaron@brethorsting.com>
2012-12-11 13:58:11 -0800

    license

--- ColorArt/PCFadedImageView.h
+++ ColorArt/PCFadedImageView.h
@@ -2,9 +2,15 @@
 //  PCFadedImageView.h
 //  ColorArt
 //
-//  Created by Wade Cosgrove on 12/4/12.
-//  Copyright (c) 2012 Wade Cosgrove. All rights reserved.
+// Copyright (C) 2012 Panic Inc. Code by Wade Cosgrove. All rights reserved.
 //
+// Redistribution and use, with or without modification, are permitted provided that the following conditions are met:
+//
+// - Redistributions must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// - Neither the name of Panic Inc nor the names of its contributors may be used to endorse or promote works derived from this software without specific prior written permission from Panic Inc.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PANIC INC BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import <Cocoa/Cocoa.h>
 
